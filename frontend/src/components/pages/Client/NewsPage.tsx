8895ced4ffa0491d822fed98575b6b50af9674ab
HyunWeb <mattew1803@naver.com>
2025-05-13 16:16:21 +0900

    cicd 오류 찾는중

--- frontend/src/components/pages/Client/NewsPage.tsx
+++ frontend/src/components/pages/Client/NewsPage.tsx
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getNews } from "../../../api/postApi";
 
+interface NewsDataProps {
+  date: string;
+  description: string;
+  img: string;
+  link: string;
+  title: string;
+}
+
 export default function NewsPage() {
+  const [newsList, setNewsList] = useState<NewsDataProps[]>([]);
+
   useEffect(() => {
     const axiosData = async () => {
       const res = await getNews();
-      console.log(res);
+      console.log(res.message);
+      setNewsList(res.message);
     };
     axiosData();
   }, []);
-  return <div>Page</div>;
+  console.log(newsList[0].title);
+  return <div></div>;
 }
